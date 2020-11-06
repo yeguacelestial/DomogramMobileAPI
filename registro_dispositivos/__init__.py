9aecf1448162c66ab3956db3a3053531e9ca7f30
Yegua Celestial <yeguacelestial@gmail.com>
2020-11-06 02:04:40 -0600

    feat.: Handle put requests

--- registro_dispositivos/__init__.py
+++ registro_dispositivos/__init__.py
@@ -11,7 +11,7 @@ from app import app
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = shelve.open("devices.db")
+        db = g._database = shelve.open("devices.db", writeback=True)
     return db
 
 
@@ -72,6 +72,32 @@ class Dispositivo(Resource):
 
         return {'message': 'Dispositivo encontrado', 'data': shelf[identificador]}, 200
 
+    def put(self, identificador):
+        shelf = get_db()
+
+        # If the key doesn't exist in the data store, return 404
+        if not (identificador in shelf):
+            return {'message': 'Dispositivo no encontrado'}, 404
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('identificador', required=False)
+        parser.add_argument('nombre', required=False)
+        parser.add_argument('tipo_dispositivo', required=False)
+        parser.add_argument('pin_dispositivo', required=False)
+        parser.add_argument('parametros', type=dict, required=False)
+
+        # Parse the arguments into an object
+        args = parser.parse_args()
+        new_args = dict(shelf[identificador])
+        print(new_args)
+
+        for k, v in args.items():
+            if v != None:
+                new_args[k] = v
+                shelf[identificador][k] = new_args[k]
+
+        return {'message': 'Dispositivo actualizado', 'data': shelf[identificador]}, 200
+
     def delete(self, identificador):
         shelf = get_db()
 
@@ -80,4 +106,4 @@ class Dispositivo(Resource):
             return {'message': 'Dispositivo no encontrado'}, 404
 
         del shelf[identificador]
-        return '', 204
+        return {'message': f'Dispositivo eliminado: {identificador}'}, 204
