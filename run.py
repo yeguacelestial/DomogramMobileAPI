d95885c6e2357e239110d665649748e0c5b8212e
Yegua Celestial <yeguacelestial@gmail.com>
2020-11-05 21:30:41 -0600

    fix: Flask mail ugly bug

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
-from registro_dispositivos import app
+from flask_restful import Api
+from app import app, SignUp, SignIn
+
+# Microservices
+from registro_dispositivos import DispositivosList, Dispositivo
+
+
+api = Api(app)
+
+api.add_resource(SignUp, '/signup')
+api.add_resource(SignIn, '/signin')
+api.add_resource(DispositivosList, '/dispositivos')
+api.add_resource(Dispositivo, '/dispositivo/<string:identificador>')
 
 
 app.run(debug=True)
