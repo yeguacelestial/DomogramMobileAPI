4cc32019187deffea8ea0a3ba227299b7f939c21
Yegua Celestial <yeguacelestial@gmail.com>
2020-11-20 19:56:26 -0600

    fix: Read serial monitor

--- registro_dispositivos/handle_sensores.py
+++ registro_dispositivos/handle_sensores.py
@@ -1,17 +1,22 @@
 def handle_temp_hum(board, shelf, identificador):
     # Fetch temperatura
     board.write(b'O')
-    temperatura = float(board.readline().decode()[:5])
-    print(f"TEMPERATURA => {temperatura}°C")
+    # temperatura = float(board.readline().decode()[:5])
+    temperatura = board.readline().decode()
+
+    if '°C' in temperatura:
+        print(f"TEMPERATURA => {temperatura}")
+        # Replace temperatura and humedad fields
+        shelf[identificador]['parametros']['temperatura'] = float(
+            temperatura[:5])
 
     # Fetch humedad
     board.write(b'X')
-    humedad = float(board.readline().decode()[:5])
-    print(f"HUMEDAD => {humedad}%")
-
-    # Replace temperatura and humedad fields
-    shelf[identificador]['parametros']['temperatura'] = temperatura
-    shelf[identificador]['parametros']['humedad'] = humedad
+    # humedad = float(board.readline().decode()[:5])
+    humedad = board.readline().decode()
+    if '%' in humedad:
+        print(f"HUMEDAD => {humedad}")
+        shelf[identificador]['parametros']['humedad'] = float(humedad[:5])
 
     return shelf[identificador]
 
@@ -19,9 +24,10 @@ def handle_temp_hum(board, shelf, identificador):
 def handle_ultrasonico(board, shelf, identificador):
     # Fetch movimiento
     board.write(b'V')
-    movimiento = float(board.readline().decode()[:5])
-    print(f"MOVIMIENTO => {movimiento}cm")
+    movimiento = board.readline().decode()
+    if 'cm' in movimiento:
+        print(f"MOVIMIENTO => {movimiento}")
+        # Replace movimiento field
+        shelf[identificador]['parametros']['distancia'] = float(movimiento[:5])
 
-    # Replace movimiento field
-    shelf[identificador]['parametros']['distancia'] = movimiento
     return shelf[identificador]
